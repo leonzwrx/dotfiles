76fef83314470021e03deb1ad7aa31de569cecee
leonzwrx <158376547+leonzwrx@users.noreply.github.com>
2025-08-28 17:16:14 -0600

    continuing

--- .config/sway/scripts/audio_switch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pactl subscribe | while read -r line; do
-  if [[ $line == *"hdmi connected"* ]]; then
-    pactl set-default-sink "alsa_output.usb-C-Media_INC._USB_Sound_Device-00.analog-stereo"
-  elif [[ $line == *"hdmi disconnected"* ]]; then
-    pactl set-default-sink "alsa_output.pci-0000_00_1f.3.analog-stereo"
-  fi
-done
